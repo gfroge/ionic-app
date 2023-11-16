b2e724a60f088af2c2d9d392e2008951c2de7831
Vadim <timecatcher08@gmail.com>
2023-11-16 20:29:27 +0300

    init

---
+++ src/app/router.ts
@@ -0,0 +1,7 @@
+import { createWebHistory } from '@ionic/vue-router'
+import { createRouter } from '@ionic/vue-router'
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [],
+})
